2bf9a288f62c9e412da9e9f7560a20b548796ac6
Candace Williams <candace.williams94@hotmail.com>
2021-12-01 17:32:46 -0500

    upload comsuming an api project.

---
+++ src/Components/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import './App.css';
+import News from './News/News';
+import Sidenews from './News/Sidenews.js';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      news1: {
+        type: 'top-headlines',
+        query: 'sources=the-next-web'
+      },
+      news2: {
+        type: 'everything',
+        query: 'domains=bitcoin.com&language=en'
+      },
+      news3: {
+        type: 'everything',
+        query: 'domains=apple.com&language=en'
+      }
+    };
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="navbar-fixed">
+          <nav>
+            <div className="nav-wrapper pink lighten-4">
+              <a href="/"  className="bran-logo center ArticleLinks">My Feed</a>
+            </div>
+          </nav>
+        </div>
+        <div className="row">
+          <div className="col s8">
+            <News news={this.state.news1} />
+            <News news={this.state.news2} />
+          </div>
+          <div className="col s4">
+            <Sidenews news={this.state.news3}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
